3068db1bf568c26cf782714181fe7ea8705f7ab8
trwilson24 <114618720+trwilson24@users.noreply.github.com>
2023-01-31 13:42:15 -0600

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -20,13 +20,13 @@ EOF
   tags = merge({
     Name = "${local.resource_prefix.value}-ec2"
     }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"   
     git_file             = "terraform/aws/ec2.tf"
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
+    git_repo             = "terragoat"  
     yor_trace            = "347af3cd-4f70-4632-aca3-4d5e30ffc0b6"
   })
 }
@@ -46,7 +46,7 @@ resource "aws_ebs_volume" "web_host_storage" {
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
-    yor_trace            = "c5509daf-10f0-46af-9e03-41989212521d"
+    yor_trace            = "c5509daf-10f0-46af-9e03-41989212521d"  
   })
 }
 
@@ -168,7 +168,7 @@ resource "aws_subnet" "web_subnet2" {
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
-    yor_trace            = "224af03a-00e0-4981-be30-14965833c2db"
+    yor_trace            = "224af03a-00e0-4981-be30-14965833c2db"   
   })
 }
 
